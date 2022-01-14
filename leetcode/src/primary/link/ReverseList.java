9f8d2b5bcb078187bf25ef4a1900e40c7905ea12
jellmo <942560621@qq.com>
2022-01-14 19:42:44 +0800

    head plug

--- leetcode/src/primary/link/ReverseList.java
+++ leetcode/src/primary/link/ReverseList.java
@@ -8,6 +8,7 @@ package primary.link;
  * <p>
  * 思路：
  * 1. 倒排，用指针指向结点，然后反转结点的next指针
+ * 2. 创建另一个单链表
  *
  * @author mohe
  * @date 2022-01-11 20:05:39
@@ -25,5 +26,29 @@ public class ReverseList {
             current = temp;
         }
         return pre;
+    }
+
+    /**
+     * 头插法新建单链表
+     */
+    public ListNode reverseList_Two(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(-1, null);
+        ListNode cur = head;
+        while (cur != null) {
+            //记录下一个结点
+            ListNode nextTemp = cur.next;
+
+            //头插法
+            ListNode temp = dummy.next;
+            cur.next = temp;
+            dummy.next = cur;
+
+            //指针下移
+            cur = nextTemp;
+        }
+        return dummy.next;
     }
 }
